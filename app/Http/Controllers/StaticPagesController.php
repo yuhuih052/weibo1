a214419326e36a9edc21e8e573607f93d4b773a2
vagrant <vagrant@homestead>
2019-11-10 14:07:22 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,22 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Auth;
 
 class StaticPagesController extends Controller
 {
-    public function home(){
+    public function home()
+    {
 
-        return view('static_pages/home');
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(25);
+        }
+
+        return view('static_pages/home', compact('feed_items'));
 
     }
 
-    public function help(){
+    public function help()
+    {
 
         return view('static_pages/help');
 
     }
 
-    public function about(){
+    public function about()
+    {
 
         return view('static_pages/about');
 
