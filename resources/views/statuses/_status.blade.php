682bf98243d333daa5ad72cf220fdbcad56bb799
vagrant <vagrant@homestead>
2019-11-10 14:26:04 +0000

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -3,7 +3,18 @@
         <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar"/>
     </a>
     <div class="media-body">
-        <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
+        <h5 class="mt-0 mb-1">{{ $user->name }}
+            <small> / {{ $status->created_at->diffForHumans() }}</small>
+        </h5>
         {{ $status->content }}
     </div>
+
+    @can('destroy', $status)
+        <form action="{{ route('statuses.destroy', $status->id) }}" onsubmit="return confirm('您确定要删除本条微博吗？');"
+              method="POST">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-sm btn-danger">删除</button>
+        </form>
+    @endcan
 </li>
